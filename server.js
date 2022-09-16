bb86a37866d1a997c268a85edc0978a356b4d98d
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-09-15 23:33:33 -0400

    Created notesHTML.js file

--- server.js
+++ server.js
@@ -17,20 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// GET route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-GET route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
+// Localhost
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
