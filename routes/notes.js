b9ac7bcc31a08ff4d408150c389bb47d9427d4fe
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-09-16 16:32:37 -0400

    Added routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,19 +1,24 @@
 // Dependencies
-const app = require('express').Router();
-const fs = require('fs')
+const fs = require('fs');
+const express = require('express');
+const routes = require('./routes/notes');
+
+const app = express();
+app.use(express.json());
+app.use('/', routes);
 
 // NPM package
 const {readFromFile, readAndAppend, writeToFile} = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 // GET route for notes page
-app.get('/api/notes', (req, res) => {
-  readFromFile('../db/db.json', 'utf8').then(data) => {
-    res.json(JSON.parse(data))};
+app.get('/', (req, res) => {
+  if(req.method == 'GET'){
+    readFromFile('../db/db.json', 'utf8').then((data) => res.json(JSON.parse(data)));
   }
-);
+});
 
-app.post('/api/notes', (req, res) => {
+app.post('/', (req, res) => {
     let db = fs.readFileSync('..db/db.json');
     db = JSON.parse(db);
     res.json(db);
