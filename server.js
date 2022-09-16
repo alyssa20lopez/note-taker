1c0455e26065f5529cdc9027340b3b0f340b17e4
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-09-15 23:40:55 -0400

    Removed notesHTML.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
@@ -12,6 +14,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// GET route for homepage
+app.get('/', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET route for notes page
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 // Localhost
 app.listen(PORT, () =>
