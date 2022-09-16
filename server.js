8690dd46968250cec75b9b547383b46fb120ff24
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-09-16 09:50:44 -0400

    Added .gitignore

--- server.js
+++ server.js
@@ -15,16 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// GET route for homepage
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 // GET route for notes page
 app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET route for homepage
 app.get('*', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/index.html'))
 );
