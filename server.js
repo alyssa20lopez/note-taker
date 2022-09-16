dd85b36700ffaf5d461ad7f84a6fd6419c0cda72
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-09-15 21:26:29 -0400

    Created POST API routes

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const fs = require('fs');
 // const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
+const notes = require('./db/db.json');
 
 const app = express();
 
+// Middleware for parsing application/json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -31,9 +33,21 @@ app.get('/api/notes', (req, res) => {
   // Send a message to the client
   res.status(200).json(`${req.method} request received to get notes`);
 
-  // Log our request to the terminal
-  console.info(`${req.method} request received to get notes`);
+  // Returning all saved notes to JSON
+  return res.json(db);
 });
+
+// POST request to add review
+app.post('/api/notes', (req, res) => {
+  // Logging that a POST request was received
+  console.info(`${req.method} request received to add notes`);
+});
+
+// Prepare a response object back to the client
+let response;
+
+// Check if there is anything in the response body
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
